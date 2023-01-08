66a5cd98ce246fb52061200f16a11071bf68edb2
Saturns Mind <112846802+SaturnsMind@users.noreply.github.com>
2023-01-08 10:15:53 -0600

    Added two functions and updated tabbing for prints
    fullBackup() and transferBackup() were added, alongside making tabs and echos display the same tabs for legibility.

--- Peacebringer.sh
+++ Peacebringer.sh
@@ -19,13 +19,13 @@ mainMenu() {
     read -p $'\n\t'"Please input your choice: " userChoice
 
     if      [ $userChoice = '1' ]; then
-        clear && exit
+        fullBackup
     elif    [ $userChoice = '2' ]; then
         dbBackup
     elif    [ $userChoice = '3' ]; then
         webBackup
     elif    [ $userChoice = '4' ]; then
-        echo "Test 3"
+        transferBackup
     elif    [ $userChoice = '5' ]; then
         echo "Test 3"
     elif    [ $userChoice = '6' ]; then
@@ -45,7 +45,87 @@ mainMenu() {
 ############################ FUNCTIONS ############################ 
 
 fullBackup() {
+    
+    # Clears the terminal, creates a directory for where we'll store our website backup, then we'll tar the directories and move them into /home/$USER/Documents while being timestamped.
+
+    clear
+
+    # Cleans up the /tmp/Backup/ directory incase the script was executed without fully completing.
+
+    cd /tmp/ && sudo rm -rf Backup/
+
+    # Create a temporary directory that will be used throughout the script.
+
+    mkdir /tmp/Backup/ && mkdir /tmp/Backup/etc/ && mkdir /tmp/Backup/usr/
+
+
+    # Here we're asking the user to give us a Username and Password so we can iterate it into the backup process.
+
+    clear
+    printf      $'\n\t'"MariaDB backup initiated...\n"
+
+    # Ask for the Username and Password, storing those into 2 variables we've aptly created.
+
+    read -p     $'\n\t'"Please input your MariaDB Username: " userName
+    read -sp    $'\n\t'"Please input your MariaDB Password: " passWord
+
+    clear
+
+    # Print out that we are initializing the backup, and tell the user where it will be located.
+
+    echo        $'\n\t'"Backup initiated, this may take a bit and is active, please wait.."
+
+    # Use the information we've gathered earlier, and CD into the specific user directory that this is being ran inside of. If we did this in /tmp/, we'd have to make a folder there and constantly check for it.
+    # Instead, we're just storing it in their /Documents/ folder, as that is >typically< created upon user account creation.
+    # We finalzie this process by tagging the Year, Month, and Day of DB creation so we can select which one to recover from later.
+
+    cd /tmp/Backup && mysqldump --user=userName --password=passWord --lock-tables --all-databases > $(date '+%Y-%m-%d').sql
+
+    # Clear the screen upon completion, and tell the user that it has finished successfully.
+
+    clear
+    printf      $'\n\t'"The DB was successfully backed up into /tmp/, exiting..."
+    sleep 2.5
+
+    # Starts the backup process of my.cnf, NGINX, apache(HTTPD), and postfix for the mail system / SendGrid settings, then moves them in the tmp directory.
+
+    cp /etc/my.cnf /tmp/Backup/etc/ && cp /etc/php.ini /tmp/Backup/etc 
+
+    sudo cp -r /etc/nginx/ /tmp/Backup/etc/ && sudo cp -r /etc/postfix/ /tmp/Backup/etc
+
+    # Starts the backup process of the letsencrypt certs for the website's SSL
+
+    sudo cp -r /etc/letsencrypt/ /tmp/Backup/etc/
+    
+    printf      $'\n\t'"/etc/ has been backed up! Backing up the website..."
     echo
+
+    # Starts the backup process of the website, and its included files. This may take long depending on what's in there.
+
+    sudo cp -r /usr/share/nginx/ /tmp/Backup/usr/
+    
+    printf      $'\n\t'"The website has been backed up! Compressing files..."
+    echo
+
+    # Dates and compresses the /tmp/Backup/ folder for RSYNC later on, then removes the temporary /tmp/Backup/ folder.
+
+    cd /tmp/ && sudo tar -zcvf "$(date '+%Y-%m-%d')_webBackup.tar.gz" /tmp/Backup/
+
+    # Moves the folder we've created to the /tmp/ directory to be cleared whenever the server is rebooted, or when /tmp/ is usually cleared.
+
+    run cd /tmp/ && sudo rm -rf Backup/
+
+    # Clear the terminal and tell the user that the operation finished.
+
+    clear
+    
+    printf      $'\n\t'"The website and its subdirections have been backed up!"
+    echo
+
+    printf      $'\n\t'"Exiting program..."
+    sleep 1.5
+    exit
+
 }
 
 dbBackup() {
@@ -65,22 +145,18 @@ dbBackup() {
     # Print out that we are initializing the backup, and tell the user where it will be located.
 
     printf      $'\n\t'"The DB backup will be stored in /tmp/. Please wait..."
-    sleep 1.5
+    sleep 2.5
 
     # Use the information we've gathered earlier, and CD into the specific user directory that this is being ran inside of. If we did this in /tmp/, we'd have to make a folder there and constantly check for it.
     # Instead, we're just storing it in their /Documents/ folder, as that is >typically< created upon user account creation.
     # We finalzie this process by tagging the Year, Month, and Day of DB creation so we can select which one to recover from later.
 
-    cd /tmp/ && mysqldump --user=$userName --password=$passWord --lock-tables --all-databases > $(date '+%Y-%m-%d').sql
+    cd /tmp/ && mysqldump --user=userName --password=passWord --lock-tables --all-databases > $(date '+%Y-%m-%d').sql
 
     # Clear the screen upon completion, and tell the user that it has finished successfully.
 
     clear
     printf      $'\n\t'"The DB was successfully backed up into /tmp/, exiting..."
-
-    echo
-    echo
-
     sleep 2.5
 
     exit
@@ -91,9 +167,8 @@ dbRestore() {
 
     clear
 
-    echo "Scanning the /tmp/ directory for any databases..."
+    echo        "Scanning the /tmp/ directory for any databases..."
     echo
-
     sleep 1
 
     # Get a list of files in /tmp/ directory.
@@ -105,17 +180,13 @@ dbRestore() {
     index=1
 
     for file in $file_list; do
-
         echo "$index: $file"
-
         index=$((index + 1))
-
     done
 
     # Prompt user to select a file.
 
-    echo -n "Select a file (enter a number): "
-
+    echo -n     $'\n\t'"Select a file (enter a number): "
     read file_number
 
     # Store users selection into a variable.
@@ -130,12 +201,14 @@ dbRestore() {
 
     # Iterate with the information we were given, restore the DB forcefully.
 
-    clear && sudo mysql --user $userName --password --force < outputFile
+    clear && sudo mysql --user userName --password --force < outputFile
 
     # Clear the terminal and tell the user that it passed, and will exit.
 
-    clear && printf $'\n\t'"The Database was properly restored! Exiting now..."
+    clear
+    printf      $'\n\t'"The Database was properly restored! Exiting now..."
     sleep 1.5
+
     exit
 
 }
@@ -164,14 +237,14 @@ webBackup() {
 
     sudo cp -r /etc/letsencrypt/ /tmp/Backup/etc/
     
-    printf $'\n\t'"/etc/ has been backed up! Backing up the website..."
+    printf      $'\n\t'"/etc/ has been backed up! Backing up the website..."
     echo
 
     # Starts the backup process of the website, and its included files. This may take long depending on what's in there.
 
     sudo cp -r /usr/share/nginx/ /tmp/Backup/usr/
     
-    printf $'\n\t'"The website has been backed up! Compressing files..."
+    printf      $'\n\t'"The website has been backed up! Compressing files..."
     echo
 
     # Dates and compresses the /tmp/Backup/ folder for RSYNC later on, then removes the temporary /tmp/Backup/ folder.
@@ -186,10 +259,10 @@ webBackup() {
 
     clear
     
-    printf $'\n\t'"The website and its subdirections have been backed up!"
+    printf      $'\n\t'"The website and its subdirections have been backed up!"
     echo
 
-    printf $'\n\t'"Exiting program..."
+    printf      $'\n\t'"Exiting program..."
     sleep 1.5
     exit
 
@@ -199,9 +272,8 @@ webRestore() {
 
     clear
 
-    echo "Scanning the /tmp/ directory for any databases..."
+    echo        "Scanning the /tmp/ directory for any databases..."
     echo
-
     sleep 1
 
     # Get a list of files in /tmp/ directory.
@@ -213,17 +285,13 @@ webRestore() {
     index=1
 
     for file in $file_list; do
-
         echo "$index: $file"
-
         index=$((index + 1))
-
     done
 
     # Prompt user to select a file.
 
-    echo -n "Select a file (enter a number): "
-
+    echo -n     $'\n\t'"Select a file (enter a number): "
     read file_number
 
     # Store users selection into a variable.
@@ -234,38 +302,34 @@ webRestore() {
 
     # Here we're going to disable the services while we attempt to restore them.
 
-    printf $'\n\t'"Disabling services momentarily..."
+    printf      $'\n\t'"Disabling services momentarily..."
     echo
-
     sleep 1.25
 
     sudo systemctl stop nginx postfix mariadb memcached.service
 
-    printf $'\n\t'"Services have successfully been disabled. Attempting restoration, please wait..."
+    printf      $'\n\t'"Services have successfully been disabled. Attempting restoration, please wait..."
     echo
-
     sleep 1.25
 
     # Here we're beginning to decompress the file we created, and moved, earlier. This contains everything we need to properly setup the new server.
 
-    printf $'\n\t'"Attempting to decompress the file, please wait..."
+    printf      $'\n\t'"Attempting to decompress the file, please wait..."
     echo
-
     sleep 2
 
     cd /tmp/ && sudo tar -xz outputFile
     clear
 
-    printf $'\n\t'"The file has successfully been decompressed! Attempting restore..."
+    printf      $'\n\t'"The file has successfully been decompressed! Attempting restore..."
     echo
-
     sleep 1.25
 
     # Here we're going to move the files to the proper directory that they came from.
 
     cd /tmp/tmp/Backup/etc && sudo cp my.cnf /etc/ && sudo cp php.ini /etc/ && sudo cp -r nginx/ /etc/ && sudo cp -r postfix/ /etc/
 
-    printf $'\n\t'"/etc/ folders have successfully been restored! Attempting website restore..."
+    printf      $'\n\t'"/etc/ folders have successfully been restored! Attempting website restore..."
     echo
     sleep 1.25
 
@@ -275,7 +339,7 @@ webRestore() {
     cd /usr/share && sudo mv nginx/ /tmp/
     cd /tmp/tmp/Backup/usr && sudo cp -r nginx/ /usr/share/
 
-    printf $'\n\t'"Website has successfully been restored! Attempting API key restore..."
+    printf      '\n\t'"Website has successfully been restored! Attempting API key restore..."
     echo
     sleep 1.25
 
@@ -283,30 +347,28 @@ webRestore() {
 
     sudo postmap /etc/postfix/sasl_passwd
 
-    printf $'\n\t'"API keys have successfully been restored! Attempting SSL certs restore..."
+    printf      $'\n\t'"API keys have successfully been restored! Attempting SSL certs restore..."
     echo
-
     sleep 1.25
 
     cd /tmp/tmp/Backup/etc/ && sudo cp -r letsencrypt/ /etc/
 
-    printf $'\n\t'"SSL certs have successfully been restored!"
+    printf      $'\n\t'"SSL certs have successfully been restored!"
     echo
-
     sleep 1.25
 
     # Now we'll start the services again, and enable them to persist on reboot
 
-    printf $'\n\t'"Starting services, and enabling them for future reboots, please wait..."
+    printf      $'\n\t'"Starting services, and enabling them for future reboots, please wait..."
     echo
 
-    sudo systemctl start nginx postfix mariadb memcached.service && sudo systemctl enable nginx postfix mariadb memcached.service
+    sudo systemctl sart nginx postfix mariadb memcached.service && sudo systemctl enable nginx postfix mariadb memcached.service
 
     # Clearing the shell and telling the user that the process is complete.
 
     clear
 
-    printf $'\n\t'"Web Restore has successfully completed! Exiting now..."
+    printf      $'\n\t'"Web Restore has successfully completed! Exiting now..."
     sleep 1.25
     clear
     exit
@@ -317,57 +379,43 @@ transferBackup() {
     
     clear
 
-    read -p     $'\n\t'"Please input the target Username: " userName
-    read -p     $'\n\t'"Please input the target IP Address: " ipAddress
-    echo
-
+    read -p     $'\n\t'"Please input the Username: " userName
+    read -p     $'\n\t'"Please input the IP: " ipAddress
     clear
-
-    printf      $'\n\t'"Entered information: "
-    echo
-    printf      $'\n\t'"Username: " $userName
-    printf      $'\n\t'"IP Address: " $ipAddress
-    echo
-    
-    printf      $'\n\t'"Are those correct?"
     echo
 
-    echo        "1: Yes"
-    echo        "2: No"
+    echo        $'\n\t'"Are these correct?"
+    echo        $'\n\t'"Username: " $userName
+    echo        $'\n\t'"IP Address: "   $ipAddress
+    echo
 
-    read -p     $'\n\t'"Response: " userChoice
+    echo        $'\n\t'"1) Yes"
+    echo        $'\n\t'"2)  No"
+    echo
+
+    read -p     $'\n\t'"Answer: "   userChoice
 
 
-    if  [ $userChoice = '1' ]; then
+    if [ $userChoice = '1' ]; then
         
         clear
 
-        scanDirectory() {
-
-        printf $'\n\t'"Scanning /tmp/ for .tar.gz files..."
-        echo
-        sleep 1.25
-
         # Get a list of files in /tmp/ directory.
 
-        file_list=$(ls /tmp/ | grep '.tar.gz' )
+        file_list=$(ls /tmp/ | grep -E '.sql|.tar.gz|_webBackup.tar.gz|.log|_link')
 
         # Print out each file in the directory with a number.
 
         index=1
 
         for file in $file_list; do
-
             echo "$index: $file"
-
             index=$((index + 1))
-
         done
 
         # Prompt user to select a file.
 
-        echo -n "Select a file (enter a number): "
-
+        echo -n $'\n\t'"Select a file (enter a number): "
         read file_number
 
         # Store users selection into a variable.
@@ -376,56 +424,35 @@ transferBackup() {
 
         clear
 
-        printf      $'\n\t'"Selected file: " $outputFile
+        echo        $'\n\t'"Preparing to transfer the file, please wait..."
+        sleep       1.5
+        clear
+
+        echo        $'\n\t'"Transferring file now..."
         echo
 
-        printf      $'\n\t'"Is this the one you wanted to transfer?"
-        echo
-
-        echo        "1: Yes"
-        echo        "2: No"
-
-        read -p     $'\n\t'"Response: " userChoice
-
-        if  [ $userChoice = '1' ]; then
-
-            if [ $outputFile -lt $i ] && [ $outputFile -gt 0 ]; then
-
-                # Now we're going to RSYNC our selected file to the destination that we've chosen earlier.
-
-                cd /tmp/ && sudo rsync -av -P outputFile userName + '@' + ipAddress + ':/tmp/'
-                echo
-
-                printf  $'\n\t'"RSYNC was successful! Exiting program now..."
-                sleep 1.25
-                exit
-
-            else
-
-                # Have the user return back and select a valid option.
-
-                echo "Invalid selection, please try again..."
-                scanDirectory
-
-            }
-        
-        elif    [ $userChoice = '2' ]; then
-
-            clear
-            printf  $'\n\t'"Returning back to selection..."
-            sleep 1.25
-            scanDirectory
-        
-        fi
-
-    elif    [ $userChoice = '2' ]; then
+        cd  /tmp && sudo rsync -av -P outputFile + '' + $userName + '@' + $ipAddress + ':/tmp/'
 
         clear
-        echo    "Returning back to the beginning, please wait..."
-        sleep 1.25
+
+        echo        $'\n\t'"File has been successfully transferred, exiting now..."
+        sleep 1.5
+        exit
+
+
+    elif [ $userChoice = '2' ]; then
+        
+        clear
+
+        printf      $'\n\t'"Returning back to choices..."
+        
+        sleep       1.5
 
         transferBackup
+
     fi
+
+
 
 }
 
@@ -436,6 +463,7 @@ serverSetup() {
 fullRestore() {
     echo
 }
+
 
 ############################ FUNCTIONS ############################
 
