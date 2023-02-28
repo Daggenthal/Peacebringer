30f81ab697de5063aef52b96b05849f711ced48e
Daggenthal <112846802+Daggenthal@users.noreply.github.com>
2023-02-28 04:11:51 -0600

    Added an echo and removed mainMenu()

--- Peacebringer.sh
+++ Peacebringer.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo
+echo "Please re-run this with -h to see available options; Use CTRL+C to exit."
+
 Help()  {
    
     # Display Help
@@ -15,45 +18,6 @@ Help()  {
     echo     $'\n\t'"wbr  --      This attempts to restore the NGINX and webBackup that a user selects in the /tmp/ directory."
     echo
 
-}
-
-mainMenu() {
-
-    clear
-
-    printf      "\n\tWelcome to Peacebringer! Please input your selection: \n"
-    
-    printf      "\n\t1: Full Backup"
-    printf      "\n\t2: Database Backup"
-    printf      "\n\t3: Website / NGINX Backup\n"
-    printf      "\n\t4: Transfer Backup"
-    printf      "\n\t5: Server Setup\n"
-    printf      "\n\t6: Restore Full Backup"
-    printf      "\n\t7: Restore Database"
-    printf      "\n\t8: Restore Website\n"
-    printf      "\n\t9: Exit\n\t"
-
-    read -p     $'\n\t'"Please input your choice: " userChoice
-
-    if      [ $userChoice = '1' ]; then
-        fullBackup
-    elif    [ $userChoice = '2' ]; then
-        dbBackup
-    elif    [ $userChoice = '3' ]; then
-        webBackup
-    elif    [ $userChoice = '4' ]; then
-        transferBackup
-    elif    [ $userChoice = '5' ]; then
-        serverSetup
-    elif    [ $userChoice = '6' ]; then
-        fullRestore
-    elif    [ $userChoice = '7' ]; then
-        dbRestore
-    elif    [ $userChoice = '8' ]; then
-        webRestore
-    elif    [ $userChoice = '9' ]; then
-        clear && exit
-    fi
 }
 
 ############################ FUNCTIONS ############################
@@ -705,7 +669,3 @@ while [ -n "$0" ]; do
 	shift
 	
 done
-
-#	Executes the main menu after everything has been set-and-done
-
-mainMenu
